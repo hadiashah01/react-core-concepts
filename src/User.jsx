5b25b8171f40ea479699c1dfae345e2bd1a70624
Hadia <hadiashah010@gmail.com>
2026-07-22 03:33:11 +0500

    create a User component having a table displaying user info + remove extra files

--- src/User.jsx
+++ src/User.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function User(info) {
   return (
     <>
+      <table border={1}>
+        <th>
+          <td>Name</td>
+          <td>Age</td>
+          <td>Date of Birth</td>
+          <td>City</td>
+          <td>Profession</td>
+        </th>
+        <tbody><td>info.name</td>
+        <td>info.age</td>
+        <td><time datetime="2006-02-10">info.dob</time></td>
+        <td>info.city</td>
+        <td>info.profession</td></tbody>
+      </table>
     </>
   )
 }
 
-export default App
+export default User
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './User.jsx'
+import User from './User.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <User/>
   </StrictMode>,
 )
