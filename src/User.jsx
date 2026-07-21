b002137ac1884c1056f2f50d3cdd373dfa86a6b7
Hadia <hadiashah010@gmail.com>
2026-07-22 04:40:37 +0500

    define age and eligibility state, add checkStatus click handlers

--- src/User.jsx
+++ src/User.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
 function User({ info }) {
+  const [age, setAge] = useState(info.age);
+  const eligibility = "...?";
+  const [eligibilityStatus, setEligibilityStatus] = useState(eligibility);
+  function checkStatus() {
+    if (age > 18 && age < 25) {
+      setEligibilityStatus("Eligible");
+    } else {
+      setEligibilityStatus("Not Eligible");
+    }
+  }
   return (
     <>
       <table border={1}>
@@ -17,7 +28,7 @@ function User({ info }) {
           <tr>
             <td>{info.name}</td>
             <td>{info.surname}</td>
-             <td>{info.country}</td>
+            <td>{info.country}</td>
             <td>{info.age}</td>
             <td>
               <time dateTime="2006-02-10">{info.dob}</time>
@@ -27,8 +38,8 @@ function User({ info }) {
           </tr>
         </tbody>
       </table>
+      <button onClick={checkStatus}>Eligibilty Status: {eligibilityStatus}</button>
     </>
   );
 }
-
 export default User;
